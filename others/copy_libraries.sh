033027a259cca4f5a6ca701a05e17d1921d5aae0
JoSH@LGECNN <seunghyeon.jo@lge.com>
2021-07-27 10:27:52 +0900

    arrange into ims_script

--- others/copy_libraries.sh
+++
@@ -1,1 +0,0 @@
-yes | cp -rf ~/repos/ambarella/integration/craft/platform/cmake_CV25_aarch64_linux/Release/install/* ~/repos/ambarella/ims_repo/lib/craft/
---
+++ others/ims_scripts/02_copy_libraries.sh
@@ -0,0 +1,1 @@
+yes | cp -rf integration/craft/platform/cmake_CV25_aarch64_linux/Release/install/* ims_repo/lib/craft/
