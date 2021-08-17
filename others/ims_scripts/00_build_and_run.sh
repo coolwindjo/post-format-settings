82b3d625b27f7fd51c407350b2496d3aadfb8d3b
JoSH@LGECNN <seunghyeon.jo@lge.com>
2021-08-17 16:24:14 +0900

    add sftp scripts

---
+++ others/ims_scripts/00_build_and_run.sh
@@ -0,0 +1,6 @@
+./01_build_craft_lib.sh
+./02_copy_libraries.sh
+./03_build_ims_repo.sh
+./04_sftp_to_board.sh
+./05_ssh_board.sh
+
---
+++ others/ims_scripts/04_sftp_to_board.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sftp -v root@192.168.0.16 <<EOF
+cd /tmp
+put engineLoadDescription.json
+put run_rpmain.sh
+lcd ims_repo/_out/ambarella/debug/
+put opt.tar.gz
+quit
+EOF
+
+ssh -v root@192.168.0.16 <<EOF
+cd /tmp
+tar xvf opt.tar.gz -C /opt
+mv engineLoadDescription.json /opt/bin/bin/
+mv run_rpmain.sh /opt/bin/
+exit
+EOF
