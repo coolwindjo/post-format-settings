75548e8caf650c7359f2e7d5825c0616176e73a2
JoSH@Vincenzo <seunghyeon.jo@lge.com>
2021-11-30 22:04:11 +0900

    add purge cuda and nvidia && install pytorch stable

--- 06_install_nvidia-driver_reboot_rqrd.sh
+++ 06_install_nvidia-driver_reboot_rqrd.sh
@@ -3,6 +3,9 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+# wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-$distribution-11-0-local_11.0.2-450.51.05-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+# sudo apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 sudo apt-get update -y
 sudo apt-get -y install cuda-drivers
