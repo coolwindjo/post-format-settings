41193eb585125a647cc9f424479626b6765e3fe2
coolwindjo <coolwind@hotmail.co.kr>
2021-12-06 17:39:52 +0900

    add install nvtop

--- 01_install_vscode_docker.sh
+++ 01_install_vscode_docker.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt autoremove -y
 sudo snap install --classic code
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
