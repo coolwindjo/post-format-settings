e5e056792e93fabae631148ea79db0a52574a047
JoSH@CoolFamPC <coolwind@hotmail.co.kr>
2022-02-20 12:32:27 +0900

    make docker for 20.04

--- 00_initialize_linux.sh
+++ 00_initialize_linux.sh
@@ -1,6 +1,8 @@
 sudo apt-get update -y && sudo apt-get install -q -y --no-install-recommends \
-python3-pip \
-wget \
+	python3-pip \
+	wget \
+	build-essential gdb clang lldb g++-10 \
+	python3 \
 && sudo rm -rf /var/lib/apt/lists/*
 # vim \
 # xvfb \
--- 01_install_vscode_docker.sh
+++ 01_install_vscode_docker.sh
@@ -1,11 +1,22 @@
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt autoremove -y
 sudo snap install --classic code
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Docker Install
+  # apt-transport-https \
+sudo apt-get update -y && sudo apt-get install -q -y --no-install-recommends \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+&& sudo rm -rf /var/lib/apt/lists/*
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 apt-cache madison docker-ce
-sudo apt-get install -y docker-ce=5:20.10.3~3-0~ubuntu-bionic docker-ce-cli=5:20.10.3~3-0~ubuntu-bionic containerd.io
+# sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
+sudo apt-get install -y docker-ce=5:20.10.12~3-0~ubuntu-focal docker-ce-cli=5:20.10.12~3-0~ubuntu-focal containerd.io
+#sudo apt-get install -y docker-ce=5:20.10.3~3-0~ubuntu-bionic docker-ce-cli=5:20.10.3~3-0~ubuntu-bionic containerd.io
