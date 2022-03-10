61ce0982a0c6a2abfe2e13aef1201717f97fc4ed
JoSH@CoolFamPC <coolwind@hotmail.co.kr>
2022-03-10 13:51:15 +0900

    fix unknown error on CRD

--- 05_fix_unknown_error_for_CRD_reboot_rqrd.sh
+++ 05_fix_unknown_error_for_CRD_reboot_rqrd.sh
@@ -1,4 +1,7 @@
 /opt/google/chrome-remote-desktop/chrome-remote-desktop --stop
 sudo rm -rf /opt/google/chrome-remote-desktop/
-# sudo rm -rf ~/.cache/
-/opt/google/chrome-remote-desktop/chrome-remote-desktop --start
+sudo rm -rf ~/.config/chrome-remote-desktop/
+sudo rm -rf ~/.cache/
+sudo dpkg -i chrome-remote-desktop_current_amd64.deb
+sudo apt-get install -f -y
+mkdir -p ~/.config/chrome-remote-desktop
