79a0a0724593bb1243fda3b152b6f0ae97d58722
JoSH@CoolFamPC <coolwind@hotmail.co.kr>
2022-03-10 12:13:27 +0900

    update chrome-remote-desktop

--- 04_install_CRD_reboot_rqrd.sh
+++ 04_install_CRD_reboot_rqrd.sh
@@ -4,5 +4,5 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb
 sudo apt-get install -f -y
 mkdir -p ~/.config/chrome-remote-desktop
-# sudo addgroup chrome-remote-desktop
-# sudo usermod -a -G chrome-remote-desktop $USER
+sudo addgroup chrome-remote-desktop
+sudo usermod -a -G chrome-remote-desktop $USER
---
+++ 05_fix_unknown_error_for_CRD_reboot_rqrd.sh
@@ -0,0 +1,4 @@
+/opt/google/chrome-remote-desktop/chrome-remote-desktop --stop
+sudo rm -rf /opt/google/chrome-remote-desktop/
+# sudo rm -rf ~/.cache/
+/opt/google/chrome-remote-desktop/chrome-remote-desktop --start
