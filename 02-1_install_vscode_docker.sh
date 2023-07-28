1044f22a00f33d23e13ed6840a805fd8200ecb50
JoSH@JoSH-WSL <jo.sh@autocrypt.io>
2023-07-28 23:23:53 +0900

    pji1

--- 02-1_install_vscode_docker.sh
+++ 02-1_install_vscode_docker.sh
@@ -18,7 +18,7 @@ sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 apt-cache madison docker-ce
 # sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io
-sudo apt-get install -y docker-ce=5:24.0.2-1~ubuntu.22.04~jammy docker-ce-cli=5:24.0.2-1~ubuntu.22.04~jammy containerd.io
+# sudo apt-get install -y docker-ce=5:24.0.2-1~ubuntu.22.04~jammy docker-ce-cli=5:24.0.2-1~ubuntu.22.04~jammy containerd.io
 # sudo apt-get install -y docker-ce=5:20.10.18~3-0~ubuntu-focal docker-ce-cli=5:20.10.18~3-0~ubuntu-focal containerd.io
 # sudo apt-get install -y docker-ce=5:20.10.18~3-0~ubuntu-bionic docker-ce-cli=5:20.10.18~3-0~ubuntu-bionic containerd.io
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
